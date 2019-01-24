04485c1e24e9d60a93ceb4c4cabf8e22f6ba8253
daniel <dany_the_man@hotmail.com>
2019-01-24 20:32:36 +0000

    BlockChain working, still need some extra implementation

--- BlockChain/BlockChain.py
+++ BlockChain/BlockChain.py
@@ -5,6 +5,11 @@
 import hashlib
 import json
 from time import time
+from uuid import uuid4
+from textwrap import dedent
+
+from flask import Flask
+from flask import jsonify
 
 #Class is responsible for managing the chain, storing transactions and contains some helper methods.
 class Blockchain(object):
@@ -66,7 +71,56 @@ class Blockchain(object):
         block_string = json.dump(block, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
 
+# Simple Proof of work Algortihm
+#find a number p' such that hash(pp') contains leading 4 zeroes, where p is previous p'
+#p is the previous proof, and p' is the new proof
+#   param last_proof: <int>
+#   return: <int>
+    def proof_of_work(self, last_proof):
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
 
+        return proof
+
+#Validates the Proof: does hash(last_proof, proof) contain 4 leading zeroes?
+#Its possible to add more zeroes but this would cause longer times to validate the proof
+#   param last_proof: <int> Previous proof
+#   param proof: <int> Current proof
+#   return: <bool> true if correct, false if not
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+
+#instantiate the node
+app = Flask(__name__)
+
+#generate a globallt unique address for this node
+node_identifier = str(uuid4()).replace('-','')
+
+#Instantiate BlockChain
+blockchain = Blockchain()
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    return "Mining a  new block"
+
+@app.route('/transactions/new', methods=['POST'])
+def new_tranaction():
+    return "Adding a new transaction"
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain' : blockchain.chain,
+        'length' : len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
 
 ''' Example of what a single block would look like and what it should contain
 block = {
